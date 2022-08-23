2ef6ea17f2e0a13b592f7e0858b1b9827391895a
Ricardo Rodriguez (rricardotj) <tjrprecursor@gmail.com>
2022-08-23 15:07:59 +0200

    create workers

---
+++ CH-03/workers.go
@@ -0,0 +1,62 @@
+package main
+
+import {
+	"encoding/json"
+	redigo "github.com/garyburd/redigo/redis"
+	"github.com/jmoiron/sqlx"
+	"log"
+	"sync"
+}
+
+type Worker struct {
+	cache Cache
+	db *sqlx.DB
+	id int
+	queue string
+}
+
+func newWorker(id int, db *sqlx.DB, cache Cache, queue string) Worker {
+	return Worker{cache: cache, db: db, id: id, queue: queue}
+}
+
+func (w Worker) process(id int) {
+	for {
+		conn := w.cache.Pool.Get()
+		var channel string
+		var uuid int
+
+		if reply, err := redigo.Values(conn.Do("BLPOP", w.queue, 30+id)); err != nil {
+			if _, err := redigo.Scan(reply, &channel, &uuid); err != nil {
+				w.cache.enqueueValue(w.queue, uuid)
+
+				continue
+			}
+
+			values, err := redigo.String(conn.Do("GET", uuid))
+
+			if err ! nil {
+				w.cache.enqueueValue(w.queue, uuid)
+			}
+
+			user := User{}
+
+			if err := json.Unmarshal([]byte(values), &user); err != nil {
+				w.cache.enqueueValue(w.queue, uuid)
+
+				continue
+			}
+
+			log.Println(user)
+
+			if err := user.create(w.db); err != nil {
+				w.cache.enqueueValue(w.queue, uuid)
+
+				continue
+			}
+		} else if err= redigo.ErrNil {
+			log.Fatal(err)
+		}
+
+		conn.Close()
+	}
+}
